4c73e3deb102e3537d37dbd34810d39ba7bea879
Andrea Ottaviani <andrea.ottaviani.69@gmail.com>
2015-09-18 10:57:05 +0200

    another step to finish....

--- src/win_snupy.c
+++ src/win_snupy.c
@@ -1,9 +1,6 @@
 #include <pebble.h>
 #include "win_snupy.h"
 
-#include "simple_analog.h"
-
-
 
 	// ricorda di aggiungere : bitmap_layer_set_compositing_mode(s_bitmaplayer_1, GCompOpSet);
 	// per la trasparenza!!!
@@ -11,8 +8,6 @@
 static Layer *s_simple_bg_layer, *s_date_layer, *s_hands_layer;
 static TextLayer *s_day_label, *s_num_label;
 
-static GPath *s_tick_paths[NUM_CLOCK_TICKS];
-static GPath *s_minute_arrow, *s_hour_arrow;
 static char s_num_buffer[4], s_day_buffer[6];
 
 	
@@ -21,10 +16,9 @@ static Window *s_window;
 static GBitmap *s_res_img_minuti;
 static GBitmap *s_res_img_snupy;
 static GBitmap *s_res_img_ore;
-static BitmapLayer *s_bitmaplayer_minuti;
+static RotBitmapLayer *s_bitmaplayer_minuti;
 static BitmapLayer *s_bitmaplayer_1;
-static BitmapLayer *s_bitmaplayer_ore;
-static GColor bkColore;
+static RotBitmapLayer *s_bitmaplayer_ore;
 static GColor Colore;
 
 static void initialise_ui(void) {
@@ -39,30 +33,31 @@ static void initialise_ui(void) {
   s_res_img_minuti = gbitmap_create_with_resource(RESOURCE_ID_IMG_MINUTI);
   s_res_img_snupy = gbitmap_create_with_resource(RESOURCE_ID_IMG_SNUPY);
   s_res_img_ore = gbitmap_create_with_resource(RESOURCE_ID_IMG_ORE);
-  // s_bitmaplayer_minuti
-  s_bitmaplayer_minuti = bitmap_layer_create(GRect(75, 72, 58, 40));
-  bitmap_layer_set_bitmap(s_bitmaplayer_minuti, s_res_img_minuti);
-	bitmap_layer_set_compositing_mode(s_bitmaplayer_minuti, GCompOpSet);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_bitmaplayer_minuti);
   
-  // s_bitmaplayer_1
+	// s_bitmaplayer_1
   s_bitmaplayer_1 = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_bitmaplayer_1, s_res_img_snupy);
 	bitmap_layer_set_compositing_mode(s_bitmaplayer_1, GCompOpSet);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_bitmaplayer_1);
   
+  // s_bitmaplayer_minuti
+  s_bitmaplayer_minuti = rot_bitmap_layer_create(s_res_img_minuti);
+  rot_bitmap_layer_set_corner_clip_color(s_bitmaplayer_minuti, GColorClear);
+  rot_bitmap_set_compositing_mode(s_bitmaplayer_minuti, GCompOpSet);
+	layer_add_child(window_get_root_layer(s_window), (Layer *)s_bitmaplayer_minuti);
+  
   // s_bitmaplayer_ore
-  s_bitmaplayer_ore = bitmap_layer_create(GRect(20, 72, 58, 40));
-  bitmap_layer_set_bitmap(s_bitmaplayer_ore, s_res_img_ore);
-	bitmap_layer_set_compositing_mode(s_bitmaplayer_ore, GCompOpSet);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_bitmaplayer_ore);
+  s_bitmaplayer_ore = rot_bitmap_layer_create(s_res_img_ore);
+  rot_bitmap_layer_set_corner_clip_color(s_bitmaplayer_ore, GColorClear);
+  rot_bitmap_set_compositing_mode(s_bitmaplayer_ore, GCompOpSet);
+	layer_add_child(window_get_root_layer(s_window), (Layer *)s_bitmaplayer_ore);
 }
 
 static void destroy_ui(void) {
   window_destroy(s_window);
-  bitmap_layer_destroy(s_bitmaplayer_minuti);
+  rot_bitmap_layer_destroy(s_bitmaplayer_minuti);
   bitmap_layer_destroy(s_bitmaplayer_1);
-  bitmap_layer_destroy(s_bitmaplayer_ore);
+  rot_bitmap_layer_destroy(s_bitmaplayer_ore);
   gbitmap_destroy(s_res_img_minuti);
   gbitmap_destroy(s_res_img_snupy);
   gbitmap_destroy(s_res_img_ore);
@@ -74,69 +69,35 @@ static void handle_window_unload(Window* window) {
 }
 
 
-
-static void bg_update_proc(Layer *layer, GContext *ctx) {
-  graphics_context_set_fill_color(ctx, GColorYellow);
-  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
-  graphics_context_set_fill_color(ctx, GColorClear);
-  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
-    gpath_draw_filled(ctx, s_tick_paths[i]);
-  }
-}
-
 static void hands_update_proc(Layer *layer, GContext *ctx) {
-  GRect bounds = layer_get_bounds(layer);
-  GPoint center = grect_center_point(&bounds);
-  //int16_t second_hand_length = bounds.size.w / 2;
 
+	
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
-  //int32_t second_angle = TRIG_MAX_ANGLE * t->tm_sec / 60;
-  //GPoint second_hand = {
-  //  .x = (int16_t)(sin_lookup(second_angle) * (int32_t)second_hand_length / TRIG_MAX_RATIO) + center.x,
-  //  .y = (int16_t)(-cos_lookup(second_angle) * (int32_t)second_hand_length / TRIG_MAX_RATIO) + center.y,
-  //};
-
   
   // minute/hour hand
-  // graphics_context_set_fill_color(ctx, GColorClear);
-  // graphics_context_set_stroke_color(ctx, GColorBlack);
 
-  // gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
-  // gpath_draw_filled(ctx, s_minute_arrow);
-  // gpath_draw_outline(ctx, s_minute_arrow);
+	//  "ORE"
+  GPoint src_ic = GPoint(48,20);
 
-  // gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
-  // gpath_draw_filled(ctx, s_hour_arrow);
-  // gpath_draw_outline(ctx, s_hour_arrow);
-
-	// Get image center "ORE"
-  GRect img_bounds = gbitmap_get_bounds(s_res_img_ore);
-  // GPoint src_ic = grect_center_point(&img_bounds);
-	GPoint src_ic = GPoint(48,20);
-	
-
-  // Get context center "ORE"
-  GRect ctx_bounds = layer_get_bounds(layer);
-  // GPoint ctx_ic = grect_center_point(&ctx_bounds);
-  GPoint ctx_ic = GPoint(72,96);
   // Angle of rotation
   int angle = (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6);
 
-	bitmap_layer_set_compositing_mode(s_bitmaplayer_ore, GCompOpSet);
-  // Draw!
-  graphics_draw_rotated_bitmap(ctx, s_res_img_ore, src_ic, angle, ctx_ic);
-	  
-	// Get context center "Minuti"
+	// Draw!
+  rot_bitmap_set_src_ic(s_bitmaplayer_ore, src_ic);
+	rot_bitmap_layer_set_angle(s_bitmaplayer_ore,angle);
+
+	
+	//  "Minuti"
   
   // Angle of rotation
   angle = (TRIG_MAX_ANGLE * t->tm_min / 60);
   src_ic = GPoint(10,20);
 	
-	bitmap_layer_set_compositing_mode(s_bitmaplayer_minuti, GCompOpSet);
   // Draw!
-  graphics_draw_rotated_bitmap(ctx, s_res_img_minuti, src_ic, angle, ctx_ic);
-	
+	rot_bitmap_set_src_ic(s_bitmaplayer_minuti, src_ic);
+	rot_bitmap_layer_set_angle(s_bitmaplayer_minuti,angle);
+  
 }
 
 static void date_update_proc(Layer *layer, GContext *ctx) {
@@ -165,41 +126,19 @@ static void window_unload(Window *s_window) {
 }
 
 static void init() {
- // window = window_create();
- // window_set_window_handlers(window, (WindowHandlers) {
- //   .load = window_load,
- //   .unload = window_unload,
- // });
- // window_stack_push(window, true);
 
   s_day_buffer[0] = '\0';
   s_num_buffer[0] = '\0';
 
-  // init hand paths
-  s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS);
-  s_hour_arrow = gpath_create(&HOUR_HAND_POINTS);
+// init hand paths
 
   Layer *window_layer = window_get_root_layer(s_window);
   GRect bounds = layer_get_bounds(window_layer);
-  GPoint center = grect_center_point(&bounds);
-  gpath_move_to(s_minute_arrow, center);
-  gpath_move_to(s_hour_arrow, center);
-
-  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
-    s_tick_paths[i] = gpath_create(&ANALOG_BG_POINTS[i]);
-  }
 
   tick_timer_service_subscribe(MINUTE_UNIT, handle_second_tick);
 }
 
 static void deinit() {
-  gpath_destroy(s_minute_arrow);
-  gpath_destroy(s_hour_arrow);
-
-  for (int i = 0; i < NUM_CLOCK_TICKS; ++i) {
-    gpath_destroy(s_tick_paths[i]);
-  }
-
   tick_timer_service_unsubscribe();
   window_destroy(s_window);
 }
@@ -208,10 +147,6 @@ static void deinit() {
 static void window_load(Window *s_window) {
   Layer *window_layer = window_get_root_layer(s_window);
   GRect bounds = layer_get_bounds(window_layer);
-
- s_simple_bg_layer = layer_create(bounds);
- layer_set_update_proc(s_simple_bg_layer, bg_update_proc);
- // layer_add_child(window_layer, s_simple_bg_layer);
 
   s_date_layer = layer_create(bounds);
   layer_set_update_proc(s_date_layer, date_update_proc);
